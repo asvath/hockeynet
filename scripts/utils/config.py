59da65d362c59d602ee45d94900913b204f2f2df
asvath <ashaasvathaman@gmail.com>
2026-01-06 20:37:04 -0500

    added some paths and fixed some paths

--- scripts/utils/config.py
+++ scripts/utils/config.py
@@ -20,8 +20,8 @@ def load_dataset_metadata() -> dict:
     Load the dataset metadata
     :return: dict containing dataset metadata
     """
-    config_path = Path(__file__).parent.parent.parent / "config" / "dataset_metadata.json"
-    with open(config_path, 'r') as f:
+    metadata_path =  PROJECT_ROOT / PATHS["DATASET_METADATA"]
+    with open(metadata_path, 'r') as f:
         return json.load(f)
 
 
@@ -30,10 +30,3 @@ PROJECT_ROOT = Path(__file__).parent.parent.parent
 
 # Load paths once at import time
 PATHS = load_paths()
-
-# Convert to absolute paths relative to project root
-RAW_VIDEO_DIR = PROJECT_ROOT / PATHS["RAW_VIDEO_DIR"]
-RAW_ANNOTATIONS_DIR = PROJECT_ROOT / PATHS["RAW_ANNOTATIONS_DIR"]
-PROCESSED_FRAMES_DIR = PROJECT_ROOT / PATHS["PROCESSED_FRAMES_DIR"]
-PROCESSED_ANNOTATIONS_DIR = PROJECT_ROOT / PATHS["PROCESSED_ANNOTATIONS_DIR"]
-LOGS_DIR = PROJECT_ROOT / PATHS["LOGS_DIR"]
